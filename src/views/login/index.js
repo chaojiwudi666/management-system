8c7742ea0389df014e47dc56e8833722eecdf454
dongshuhao <746564612@qq.com>
2020-03-24 17:49:01 +0800

    firstcommit

---
+++ src/views/login/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+
+  render () {
+    return (
+      <div className="LoginPage">
+          login
+      </div>
+    );
+  }
+
+}
+
+export default Login;
